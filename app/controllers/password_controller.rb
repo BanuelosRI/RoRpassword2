6254cbdc2949be40c6a2918b16a3a5c998101f85
learn <learn@learn16.attlocal.net>
2016-10-18 15:28:31 -0700

    Adding a counter to our password checker with rails

--- app/controllers/password_controller.rb
+++ app/controllers/password_controller.rb
@@ -2,14 +2,23 @@ class PasswordController < ApplicationController
 
   def check
 
+    if cookies[:count].nil?
+      cookies[:count] = 0
+    end
+
+    @counter = cookies[:count]
+
     @user_id = params[:userid]
     @password = params[:password]
 
     if (user_valid == true) && (password_valid == true)
       @result = "Credentials are acceptable"
     else
+      cookies[:count] = @counter.to_i + 1
+      @counter = cookies[:count]
       @result = "Try again!! You entered username: #{@user_id} and password: #{@password}."
     end
+
 
   end
 
@@ -21,6 +30,12 @@ class PasswordController < ApplicationController
   #this checks if the password is valid, with 7 charachters and must include ! orv $
   def password_valid
     @password.length >= 7 && (@password.include?("!") || @password.include?("$"))
+  end
+
+  def reset
+    cookies[:count] = 0
+    @counter = cookies[:count]
+    render "check.html.erb"
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get '/check_password' => 'password#check'
+
+  get '/new_game' => 'password#reset'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
